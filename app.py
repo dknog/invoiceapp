55061e6a6880e655a1272d1ca1e78caff87a1809
Allan Eising <allan@eising.dk>
2020-02-02 15:07:25 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from flask import(
+    Flask,
+    render_template,
+    request,
+    abort
+)
+from payments import StripeInvoice, CustomerNotFoundException, ChargeNotFoundException
+from dotenv import load_dotenv
+from flask_wkhtmltopdf import Wkhtmltopdf
+
+
+APP_ROOT = os.path.dirname(__file__)
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
+
+app = Flask(__name__)
+app.config['WKHTMLTOPDF_BIN_PATH'] = os.getenv('WKHTMLTOPDF_BIN_PATH')
+app.config['PDF_DIR_PATH'] = os.path.join(APP_ROOT, 'static', 'pdf')
+wkhtmltopdf = Wkhtmltopdf(app)
+
+stripe_key = os.getenv('STRIPE_KEY')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/receipt', methods=['POST'])
+def receipt():
+
+    stripe = StripeInvoice(stripe_key)
+    email = request.form.get('email', None)
+    if email is None:
+        # TODO Proper error handling
+        abort(401)
+    receiptid = request.form.get('receiptid', None)
+    if receiptid is None:
+        # TODO Proper error handling
+        abort(401)
+
+    try:
+        receipt = stripe.generate_receipt(email, receiptid)
+    except (CustomerNotFoundException, ChargeNotFoundException):
+        return render_template('error.html')
+
+    return render_template('receipt.html', download=True, save=False, receipt=receipt, pdf=False)
+
+@app.route('/pdf/<email>/<receipt_number>')
+def pdfreceipt(email, receipt_number):
+    stripe = StripeInvoice(stripe_key)
+    receipt = stripe.generate_receipt(email, receipt_number)
+
+    return wkhtmltopdf.render_template_to_pdf('receipt.html', receipt=receipt, pdf=True)
---
+++ payments.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+import stripe
+from datetime import datetime
+from iso3166 import countries
+
+
+class CustomerNotFoundException(Exception):
+    pass
+
+
+class ChargeNotFoundException(Exception):
+    pass
+
+
+class StripeInvoice(object):
+    """ Class to fetch invoice data from stripe, given receipt number and email address """
+
+    def __init__(self, stripe_key):
+        stripe.api_key = stripe_key
+
+
+    def find_customer(self, email):
+        return stripe.Customer.list(email=email)
+
+
+    def lookup_customer(self, id):
+        return stripe.Customer.retrieve(id)
+
+
+    def find_payments(self, customer_id):
+        return stripe.Charge.list(customer=customer_id)
+
+    def find_charge_by_receipt(self, charges, receipt_number):
+        relevant_charge = None
+        for charge in charges['data']:
+            if charge['receipt_number'] == receipt_number:
+                relevant_charge = charge
+                break
+
+        return relevant_charge
+
+    def generate_receipt(self, email, receipt_number):
+        """ Generate a receipt based on email address and the receipt number"""
+        customers = self.find_customer(email)
+        customer_ids = []
+        for customer in customers['data']:
+            customer_ids.append(customer['id'])
+
+        charge = None
+        receipt = {}
+
+        if len(customer_ids) > 0:
+            for customer_id in customer_ids:
+                charges = self.find_payments(customer_id)
+                charge = self.find_charge_by_receipt(charges, receipt_number)
+                if charge is not None:
+                    country = countries.get(charge['billing_details']['address']['country'])
+                    receipt['amount'] = charge['amount'] / 100
+                    receipt['currency'] = charge['currency']
+                    receipt['timestamp'] = datetime.fromtimestamp(charge['created'])
+                    receipt['charge'] = charge
+                    receipt['country'] = country.name
+                    receipt['email'] = email
+                    receipt['receipt_number'] = receipt_number
+
+                    break
+
+        else:
+            raise CustomerNotFoundException('Customer Not Found')
+
+        if len(receipt) == 0:
+            raise ChargeNotFoundException('No charge could be found')
+
+        return receipt
+
+#            print('Invoice')
+#            print(f'Amount: {receipt["amount"]} {receipt["currency"]}')
+#            print(f'Date of purchase: {receipt["timestamp"]}')
+
+
+    #generate_receipt('eising@dknog.dk', '1681-6042')
